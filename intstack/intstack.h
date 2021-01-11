43884633c86b3e0ee8fab89e31a71288d99e854f
Pauline Delarue <pauline.delarue@mines-paristech.fr>
2021-01-11 12:22:33 +0100

    modif de intstack

--- intstack/intstack.h
+++ intstack/intstack.h
@@ -1,15 +1,70 @@
+#include <iostream>
+
 class IntStack {
-    public:
-    IntStack (int len);
-    IntStack(const IntStack& rst);
-    ~IntStack();
-    void push (int token);
-    int pop ();
-    bool is_full ();
-    bool is_empty ();
-    void print ();
-    private:
-    int *tab;
-    int taille;
-    int top;
+
+public:
+
+  IntStack (int len) : taille(len), top(0) {
+    if (len>0) {
+      tab = new int [taille];
+    }
+    else {
+      throw("taille nulle");
+    }
+  }
+
+    IntStack (const IntStack& rst): taille(rst.taille), top(rst.top), tab(new int[taille]) {
+    for (int i = 0; i < top; i++) {
+      tab[i] = rst.tab[i];
+    }
+  }
+
+  ~IntStack () {
+    delete [] tab;
+  }
+
+  void push (int token) {
+    if (not is_full()) {
+      tab[top] = token;
+      top = top + 1;
+    }
+    else {
+      throw("pile pleine");
+    }
+  }
+
+  int pop ();
+
+  bool is_full () {
+    return top == taille;
+  }
+
+  bool is_empty () {
+    return top == 0;
+  }
+
+  void print () {
+    std::cout << '[';
+    for (int k = 0; k < top; k++) {
+      std::cout << tab[k] << ' ';
+    }
+    std::cout << '[';
+  }
+
+private:
+  
+  int *tab;
+  int taille;
+  int top;
+
 };
+
+inline int IntStack::pop () {
+  if (not is_empty()) {
+    top = top - 1;
+    return tab[top];
+  }
+  else {
+    throw("pile vide");
+  }
+}
