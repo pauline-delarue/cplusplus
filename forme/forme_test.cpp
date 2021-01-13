6182e7b04d61affe12649248cf4135941589be63
Pauline Delarue <pauline.delarue@mines-paristech.fr>
2021-01-13 16:46:47 +0100

    changement des tests

--- forme/forme_test.cpp
+++ forme/forme_test.cpp
@@ -3,6 +3,8 @@
 
 int main () {
     Cercle c (2,2,1);
-    std::cout << c.area(); 
+    std::cout << c.area() << std::endl;
+    Rectangle r (5,5,3,4);
+    std::cout << r.area() << std::endl;
     return 0;
 }
